046ea4ce73d065be294324f102935df96038cef3
shanika Jayawardane <shanikauwu@gmail.com>
2025-02-16 23:38:47 -0700

    add the navBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import Navbar from "./Navbar";
+import "./Header.css";
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <h1>FilmZone</h1>
+      <Navbar />
+    </header>
+  );
 }
 
 export default Header;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageHome from "../pages/PageHome";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import About from "../pages/About";
+import Favorite from "../pages/Favorite";
+import PageNotFound from "../pages/PageNotFound";
 
 function AppRouter() {
   return (
@@ -10,6 +13,9 @@ function AppRouter() {
       <Header />
       <Routes>
         <Route path="/" element={<PageHome />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
