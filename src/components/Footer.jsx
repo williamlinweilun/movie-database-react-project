12629ade1880bddb049fc8ae3aa451407b3bfa1f
Shyanne Manuel <manuelshyanne@gmail.com>
2025-02-21 23:01:02 -0800

    added tmdb credits in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <footer>
       <h3>FilmZone</h3>
+      <p><a href="https://www.themoviedb.org/movie/top-rated?language=en-CA" target="_blank" rel="noopener noreferrer">Powered by TMDB</a></p>
       <p>&copy; 2025 by Shanika, Shyanne, William</p>
     </footer>
   );
