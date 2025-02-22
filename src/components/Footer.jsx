c278d6048523edb8139fddf1ec50c32ef4fb593a
Shyanne Manuel <manuelshyanne@gmail.com>
2025-02-21 22:27:15 -0800

    Added styles in Navigation bar and footer section, added responsiveness for nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <footer>
       <h3>FilmZone</h3>
+      <p>&copy; 2025 by Shanika, Shyanne, William</p>
     </footer>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,43 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
 
 function Navbar() {
+  const navRef = useRef();
+  const location = useLocation();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
+  useEffect(() => {
+    navRef.current.classList.remove("responsive_nav");
+  }, [location]);
+
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/favorite">Favorite</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="responsive-nav-bar">
+      <nav ref={navRef}>
+        <ul>
+          <li>
+            <NavLink to="/">MOVIES</NavLink>
+          </li>
+          <li>
+            <NavLink to="/favorite">FAVORITES</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">ABOUT</NavLink>
+          </li>
+        </ul>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars/>
+      </button>
+    </div>
   );
 }
 
