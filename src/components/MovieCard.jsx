31664843424886fa99b998f2ad3e777681187664
Shyanne Manuel <manuelshyanne@gmail.com>
2025-02-23 22:49:32 -0800

    fixing the styles in favorite button and movie cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,6 @@ import FavoriteButton from "./FavoriteButton";
 import "./MovieCard.css";
 
 function MovieCard({ movie }) {
-
   const formatReleaseDate = (dateString) => {
     const options = { year: "numeric", month: "short", day: "numeric" };
     const date = new Date(dateString);
@@ -13,22 +12,29 @@ function MovieCard({ movie }) {
 
   return (
     <>
-      <FavoriteButton movie={movie} />
-      <div className="movie-card">
-        <Link to={`/movie/${movie.id}`}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="movie-info">
-            <p className="movie-title">{movie.title}</p>
-            <p className="movie-rating">{(movie.vote_average * 10).toFixed(0)}%</p>
-            <p className="movie-details">
-              {movie.runtime && <p>⏳ Duration: {movie.runtime} minutes</p>}
-            </p>
-            <p className="movie-details">{formatReleaseDate(movie.release_date)}</p>
-          </div>
-        </Link>
+      <div className="movie-card-wrapper">
+        <FavoriteButton movie={movie}  />
+        <div className="movie-card-container">
+          <Link to={`/movie/${movie.id}`}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+              className="movie-card-poster"
+            />
+            <div className="movie-info">
+              <p className="movie-rating">
+                {(movie.vote_average * 10).toFixed(0)}%
+              </p>
+              <p className="movie-title">{movie.title}</p>
+              <p className="movie-details">
+                {movie.runtime && <p>⏳ Duration: {movie.runtime} minutes</p>}
+              </p>
+              <p className="movie-details">
+                {formatReleaseDate(movie.release_date)}
+              </p>
+            </div>
+          </Link>
+          </div>  
       </div>
     </>
   );
